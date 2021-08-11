4d2a47aa1e80ab644b6f59b275c8c809e53b2e3b
Roshani Khairnar <khairnar41997@gmail.com>
2021-08-10 22:06:05 -0700

    conversion of string to base62

---
+++ conversion/convertTo62.go
@@ -0,0 +1,46 @@
+package convertTo62
+
+import(
+	"math"
+	"strings"
+	"errors"
+	"fmt"
+)
+
+  const (
+	alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	length   = uint64(len(alphabet))
+  )
+  
+  func Encode(number uint64) string {
+	var encodedBuilder strings.Builder
+	encodedBuilder.Grow(11)
+  
+	for ; number > 0; number = number / length {
+	   encodedBuilder.WriteByte(alphabet[(number % length)])
+	}
+  
+	return encodedBuilder.String()
+  }
+  
+  func Decode(encoded string) (uint64, error) {
+	var number uint64
+  
+	for i, symbol := range encoded {
+	   alphabeticPosition := strings.IndexRune(alphabet, symbol)
+  
+	   if alphabeticPosition == -1 {
+		  return uint64(alphabeticPosition), errors.New("invalid character: " + string(symbol))
+	   }
+	   number += uint64(alphabeticPosition) * uint64(math.Pow(float64(length), float64(i)))
+	}
+  
+	return number, nil
+  }
+  
+ /* func main(){
+	  var a uint64=645
+	  fmt.Println(Encode(a))
+	  fmt.Println(Decode("rhegrfge"))
+	
+  }*/
