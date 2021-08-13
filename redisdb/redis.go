313294976b674b9c5ae8aa9f05d42ab4cffc0160
roshanikhairnar <khairnar41997@gmail.com>
2021-08-12 23:48:55 -0700

    need to check redirect function in handler

--- redisdb/redis.go
+++ redisdb/redis.go
@@ -1,129 +1,129 @@
 package redis
 
 import (
-	
 	"time"
 
+	"fmt"
+	"math/rand"
+	"strconv"
+
+	"../conversion"
+	"../operation"
 	"github.com/gomodule/redigo/redis"
-   "../operation"
-   "fmt"
-   "strconv"
-   "math/rand"
-   "../conversion"
-	
 )
-
-
 
 type redisdb struct{ pool *redis.Pool }
 
-func New(host, port, password string) (*redisdb,error){
-  pool := &redis.Pool{
-     MaxIdle:     10,
-     IdleTimeout: 240 * time.Second,
-     Dial: func() (redis.Conn, error) {
-        return redis.Dial("tcp", fmt.Sprintf("%s:%s", host, port))
-     },
-  }
+func New(host string, port string) (*redisdb, error) {
+	pool := &redis.Pool{
+		MaxIdle:     10,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", fmt.Sprintf("%s:%s", host, port))
+		},
+	}
 
-  return &redisdb{pool}, nil
+	return &redisdb{pool}, nil
 }
-
 
 func (r *redisdb) doesExist(id uint64) bool {
-  conn := r.pool.Get()
-  defer conn.Close()
+	conn := r.pool.Get()
+	defer conn.Close()
 
-  exists, err := redis.Bool(conn.Do("EXISTS", "Shortener:"+strconv.FormatUint(id, 10)))
-  if err != nil {
-     return false
-  }
-  return exists
+	exists, err := redis.Bool(conn.Do("EXISTS", "Shortener:"+strconv.FormatUint(id, 10)))
+	if err != nil {
+		return false
+	}
+	return exists
 }
-
 
 func (r *redisdb) Store(url string, expires time.Time) (string, error) {
-  conn := r.pool.Get()
-  defer conn.Close()
+	conn := r.pool.Get()
+	defer conn.Close()
 
-  var id uint64
+	var id uint64
 
-  for used := true; used; used = r.doesExist(id) {
-     id = rand.Uint64()
-  }
+	for used := true; used; used = r.doesExist(id) {
+		id = rand.Uint64()
+	}
 
-  shortLink := operation.Item{id, url, expires.Format("2006-01-02 15:04:05.728046 +0300 EEST"), 0}
+	shortLink := operation.Item{id, url, expires.Format("2006-01-02 15:04:05.728046 +0300 EEST"), 0}
+	fmt.Println("shortlink", shortLink)
+	link1, err := conn.Do("HMSET", redis.Args{"Shortener:" + strconv.FormatUint(id, 10)}.AddFlat(shortLink)...)
+	if err != nil {
+		fmt.Println("erro in storing")
+		return "", err
+	}
 
-  _, err := conn.Do("HMSET", redis.Args{"Shortener:" + strconv.FormatUint(id, 10)}.AddFlat(shortLink)...)
-  if err != nil {
-     return "", err
-  }
+	fmt.Println("HMSET return", link1)
 
-  _, err = conn.Do("EXPIREAT", "Shortener:"+strconv.FormatUint(id, 10), expires.Unix())
-  if err != nil {
-     return "", err
-  }
+	expiredTime, err := conn.Do("EXPIREAT", "Shortener:"+strconv.FormatUint(id, 10), expires.Unix())
+	if err != nil {
+		return "", err
+	}
+	fmt.Println(expiredTime)
 
-  return conversion.Encode(id), nil
+	return conversion.Encode(id), nil
 }
 
-
 func (r *redisdb) Getlink(code string) (string, error) {
-   conn := r.pool.Get()
-   defer conn.Close()
- 
-   decodedId, err := conversion.Decode(code)
-   if err != nil {
-      return "", err
-   }
- 
-   urlString, err := redis.String(conn.Do("HGET", "Shortener:"+strconv.FormatUint(decodedId, 10), "url"))
-   if err != nil {
-      return "", err
-   } else if len(urlString) == 0 {
-      return "", err
-   }
- 
-   _, err = conn.Do("HINCRBY", "Shortener:"+strconv.FormatUint(decodedId, 10), "visits", 1)
- 
-   return urlString, nil
- }
- 
- func (r *redisdb) isAvailable(id uint64) bool {
-   conn := r.pool.Get()
-   defer conn.Close()
- 
-   exists, err := redis.Bool(conn.Do("EXISTS", "Shortener:"+strconv.FormatUint(id, 10)))
-   if err != nil {
-      return false
-   }
-   return !exists
- }
- 
- func (r *redisdb) LoadInfo(code string) (*operation.Item, error) {
-   conn := r.pool.Get()
-   defer conn.Close()
- 
-   decodedId, err := conversion.Decode(code)
-   if err != nil {
-      return nil, err
-   }
- 
-   values, err := redis.Values(conn.Do("HGETALL", "Shortener:"+strconv.FormatUint(decodedId, 10)))
-   if err != nil {
-      return nil, err
-   } else if len(values) == 0 {
-      return nil, err
-   }
-   var shortLink operation.Item
-   err = redis.ScanStruct(values, &shortLink)
-   if err != nil {
-      return nil, err
-   }
- 
-   return &shortLink, nil
- }
- 
- func (r *redisdb) Close() error {
-   return r.pool.Close()
- }
+	conn := r.pool.Get()
+	defer conn.Close()
+	fmt.Println("code:", code)
+	decodedId, err := conversion.Decode(code)
+	if err != nil {
+		return "", err
+	}
+	fmt.Println("decoded link:", decodedId)
+	urlString, err := redis.String(conn.Do("HGET", "Shortener:"+strconv.FormatUint(decodedId, 10), "url"))
+	fmt.Println(urlString)
+	if err != nil {
+		return "", err
+	} else if len(urlString) == 0 {
+		return "", err
+	}
+
+	_, err = conn.Do("HINCRBY", "Shortener:"+strconv.FormatUint(decodedId, 10), "visits", 1)
+
+	return urlString, nil
+}
+
+func (r *redisdb) isAvailable(id uint64) bool {
+	conn := r.pool.Get()
+	defer conn.Close()
+
+	exists, err := redis.Bool(conn.Do("EXISTS", "Shortener:"+strconv.FormatUint(id, 10)))
+	if err != nil {
+		return false
+	}
+	return !exists
+}
+
+func (r *redisdb) LoadInfo(code string) (*operation.Item, error) {
+	conn := r.pool.Get()
+	defer conn.Close()
+
+	decodedId, err := conversion.Decode(code)
+	if err != nil {
+		return nil, err
+	}
+
+	values, err := redis.Values(conn.Do("HGET", "Shortener:"+strconv.FormatUint(decodedId, 10)))
+	fmt.Println(values)
+	if err != nil {
+		return nil, err
+	} else if len(values) == 0 {
+		return nil, err
+	}
+	var shortLink operation.Item
+	err = redis.ScanStruct(values, &shortLink)
+	if err != nil {
+		return nil, err
+	}
+
+	return &shortLink, nil
+}
+
+func (r *redisdb) Close() error {
+	return r.pool.Close()
+}
