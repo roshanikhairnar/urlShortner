e934481cc8110adf118a90cf09acf21db54505d5
Roshani Khairnar <khairnar41997@gmail.com>
2021-08-18 14:05:27 +0530

    Add files via upload

--- conversion/conversion.go
+++ conversion/conversion.go
@@ -1,77 +1,45 @@
 package conversion
-
-import (
-	"crypto/sha256"
-	"errors"
-	"fmt"
+import(
 	"math"
-	"math/big"
-	"os"
 	"strings"
-
+	"errors"
 	//"fmt"
-	"github.com/itchyny/base58-go"
 )
 
-const (
+  const (
 	alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	length   = uint64(len(alphabet))
-)
-
-func GenerateShortLink(initialLink string, userId string) string {
-	urlHashBytes := sha256Of(initialLink + userId)
-	fmt.Println("uslHshBytes", urlHashBytes)
-	generatedNumber := new(big.Int).SetBytes(urlHashBytes).Uint64()
-	finalString := base58Encoded([]byte(fmt.Sprintf("%d", generatedNumber)))
-	fmt.Println("final string:", finalString)
-	return finalString[:8]
-}
-
-func sha256Of(input string) []byte {
-	algorithm := sha256.New()
-	algorithm.Write([]byte(input))
-	return algorithm.Sum(nil)
-}
-
-func base58Encoded(bytes []byte) string {
-	encoding := base58.BitcoinEncoding
-	encoded, err := encoding.Encode(bytes)
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-	return string(encoded)
-}
-
-/* func Encode(url string) string {
-	/* var encodedBuilder strings.Builder
+  )
+  
+  func Encode(number uint64) string {
+	var encodedBuilder strings.Builder
 	encodedBuilder.Grow(11)
-
+  
 	for ; number > 0; number = number / length {
 	   encodedBuilder.WriteByte(alphabet[(number % length)])
 	}
-
+  
 	return encodedBuilder.String()
   }
-*/
-func Decode(encoded string) (uint64, error) {
+  
+  func Decode(encoded string) (uint64, error) {
 	var number uint64
-
+  
 	for i, symbol := range encoded {
-		alphabeticPosition := strings.IndexRune(alphabet, symbol)
-
-		if alphabeticPosition == -1 {
-			return uint64(alphabeticPosition), errors.New("invalid character: " + string(symbol))
-		}
-		number += uint64(alphabeticPosition) * uint64(math.Pow(float64(length), float64(i)))
+	   alphabeticPosition := strings.IndexRune(alphabet, symbol)
+  
+	   if alphabeticPosition == -1 {
+		  return uint64(alphabeticPosition), errors.New("invalid character: " + string(symbol))
+	   }
+	   number += uint64(alphabeticPosition) * uint64(math.Pow(float64(length), float64(i)))
 	}
-
+  
 	return number, nil
-}
-
-/* func main(){
+  }
+  
+ /* func main(){
 	  var a uint64=645
 	  fmt.Println(Encode(a))
 	  fmt.Println(Decode("rhegrfge"))
-
+	
   }*/
